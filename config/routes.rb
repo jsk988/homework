d454921f505b77a39d71432aac0c7ebff5092713
vagrant <vagrant@kellogg-dev-box>
2014-05-09 18:46:25 +0000

    Week 6 homework

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Homework::Application.routes.draw do
-  get '/dice/roll' => 'dice/index'
-  get '/books/list' => 'books/index'
+  get "/dice/roll" => 'dice/index'
+  get "/books/list" => 'books/index'
+
+  get "/" => 'books#home'
+  get "/books" => 'books/index'
+  get "/books/:id" => 'books/show'
 end
